348ad408938df539931c5d5917730fab36d01050
isubasinghe <i.subasinghe@unsw.edu.au>
2024-05-09 11:16:20 +1000

    added files

---
+++ virt_tx.c
@@ -0,0 +1,96 @@
+#include <stdint.h>
+#include <stdbool.h>
+#define DRIVER 0
+#define CLIENT_CH 1
+#define NUM_CLIENTS 3
+#define NET_BUFFER_SIZE 2048
+
+struct net_buff_desc {
+    /* offset of buffer within buffer memory region or io address of buffer */
+    uint64_t io_or_offset;
+    /* length of data inside buffer */
+    uint16_t len;
+};
+
+struct net_queue {
+    /* index to insert at */
+    uint32_t tail;
+    /* index to remove from */
+    uint32_t head;
+    /* flag to indicate whether consumer requires signalling */
+    uint32_t consumer_signalled;
+    /* buffer descripter array */
+    struct net_buff_desc *buffers;
+};
+
+struct net_queue_handle {
+     /* available buffers */
+    struct net_queue *free;
+     /* filled buffers */
+    struct net_queue *active;
+    /* size of the queues */
+    uint32_t size;
+};
+
+
+uintptr_t tx_free_drv;
+uintptr_t tx_active_drv;
+uintptr_t tx_free_arp;
+uintptr_t tx_active_arp;
+uintptr_t tx_free_cli0;
+uintptr_t tx_active_cli0;
+uintptr_t tx_free_cli1;
+uintptr_t tx_active_cli1;
+
+uintptr_t buffer_data_region_arp_vaddr;
+uintptr_t buffer_data_region_cli0_vaddr;
+uintptr_t buffer_data_region_cli1_vaddr;
+
+uintptr_t buffer_data_region_arp_paddr;
+uintptr_t buffer_data_region_cli0_paddr;
+uintptr_t buffer_data_region_cli1_paddr;
+
+struct state {
+    struct net_queue_handle tx_queue_drv;
+    struct net_queue_handle tx_queue_clients[NUM_CLIENTS];
+    uintptr_t buffer_region_vaddrs[NUM_CLIENTS];
+    uintptr_t buffer_region_paddrs[NUM_CLIENTS];
+};
+
+
+bool net_queue_empty_free(struct net_queue_handle *queue)
+//@ requires net_queue_handle_free(queue, _) &*& net_queue_handle_size(queue, _);
+//
+{
+
+  return queue->size != 0;
+}
+
+/* int extract_offset(uintptr_t *phys, struct state *gstate)
+{
+
+    int myphys = *phys;
+    uintptr_t *paddrs = gstate->buffer_region_paddrs;
+    uintptr_t *vaddrs = gstate->buffer_region_vaddrs;
+    struct net_queue_handle *tx_queue_clients = gstate->tx_queue_clients;
+    for (int client = 0; client < NUM_CLIENTS; client++)
+    {
+        if (myphys >= paddrs[client] &&
+            myphys <  paddrs[client] + tx_queue_clients[client].size * NET_BUFFER_SIZE) {
+            return client;
+        }
+    }
+
+    return -1;
+} */
+
+
+void init() {
+}
+
+int main(int argc, char *argv[]) 
+//@ requires true;
+//@ ensures true;
+{
+  return 0;
+}
